63fd34c229840aaba731cd6596cd0494a5dc1a31
Patrick Thomson <patrickt@github.com>
2019-05-27 17:40:19 -0400

    Remove dead comment.

--- instances/Poly/HTTP.hs
+++ instances/Poly/HTTP.hs
@@ -35,27 +35,3 @@ runHttp' = interpret $ \case
 
 runHttp :: HttpM a -> IO a
 runHttp = runM . runHttp'
-
--- open' :: String -> HttpM ()
--- open'  = send . Open
-
--- close' :: HttpM ()
--- close' = send Close
-
--- post' :: String -> HttpM String
--- post' = send . Post
-
--- get' :: HttpM String
--- get' = send Get
-
--- runHttp' :: Eff (Http ': r) w -> Eff r w
--- runHttp' (Val x) = pure x
--- runHttp' (E u q) = case decomp u of
---   Right (Open _) -> runHttp (qApp q ())
---   Right Close    -> runHttp (qApp q ())
---   Right (Post d) -> runHttp (qApp q d)
---   Right Get      -> runHttp (qApp q "")
---   Left u'        -> E u' (tsingleton (runHttp . qApp q ))
-
--- runHttp :: HttpM a -> IO a
--- runHttp = runM . runHttp'
